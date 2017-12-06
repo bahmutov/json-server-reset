7dd18fa2cbaf96f42755bbddd313519c39b8ca04
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-12-06 10:10:53 -0500

    dummy unit test

--- src/json-server-reset-spec.js
+++ src/json-server-reset-spec.js
@@ -1,10 +1,12 @@
 'use strict'
 
 /* eslint-env mocha */
+const la = require('lazy-ass')
+const is = require('check-more-types')
 const jsonServerReset = require('.')
 
 describe('json-server-reset', () => {
-  it('write this test', () => {
-    console.assert(jsonServerReset, 'should export something')
+  it('is a middleware function', () => {
+    la(is.fn(jsonServerReset))
   })
 })
