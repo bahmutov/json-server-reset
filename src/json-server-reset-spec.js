3d0f1a2d0dc2e04845cc47db1c0bbbd47f07fc5a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-12-06 09:52:51 -0500

    chore: initial code

---
+++ src/json-server-reset-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const jsonServerReset = require('.')
+
+describe('json-server-reset', () => {
+  it('write this test', () => {
+    console.assert(jsonServerReset, 'should export something')
+  })
+})
