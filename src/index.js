3f19ea6c9a5a57bdf9e60171861ec5d3d6c36be9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-10-27 20:37:41 +0100

    feat: immediately write DB to disk after reset

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ function jsonServerReset (req, res, next) {
     debug('new data %o', data)
 
     req.app.db.setState(data)
+    // and immediately write the database file
+    req.app.db.write()
+    debug('have written updated data to disk')
+
     return res.sendStatus(200)
   }
   // not a POST /reset
